df0569a2394f628b9708c1eaf10b0333d0229879
Elton Dornelas <eltondornelas@gmail.com>
2022-03-17 06:03:39 -0300

    adding the Player into State

--- src/states.rs
+++ src/states.rs
@@ -1,3 +1,4 @@
+use crate::Player;
 use bracket_lib::prelude::*;
 
 enum GameMode {
@@ -6,7 +7,13 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 pub struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
@@ -25,16 +32,36 @@ impl GameState for State {
 impl State {
     pub fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        // TODO: fill in this stub later
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
