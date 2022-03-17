47770ff91c911d522603789117452c50b8cc9598
Elton Dornelas <eltondornelas@gmail.com>
2022-03-17 07:13:13 -0300

    - adding Score into State - adding Obstacle and Score to the play function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use crate::player::Player;
 use crate::states::State;
 use bracket_lib::prelude::*;
-use crate::player::Player;
 
 mod player;
 mod states;
@@ -9,11 +9,10 @@ const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const FRAME_DURATION: f32 = 75.0;
 
-
 struct Obstacle {
     x: i32,
     gap_y: i32,
-    size: i32
+    size: i32,
 }
 
 impl Obstacle {
@@ -23,7 +22,7 @@ impl Obstacle {
         Obstacle {
             x,
             gap_y: random.range(10, 40),
-            size: i32::max(2, 20 - score)
+            size: i32::max(2, 20 - score),
         }
     }
 
@@ -33,30 +32,18 @@ impl Obstacle {
 
         // Draw the top half of the obstacle
         for y in 0..self.gap_y - half_size {
-            ctx.set(
-                screen_x,
-                y,
-                RED,
-                BLACK,
-                to_cp437('|')
-            );
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
         }
 
         // Draw the botton half of the obstacle
         for y in self.gap_y + half_size..SCREEN_HEIGHT {
-            ctx.set(
-                screen_x,
-                y,
-                RED,
-                BLACK,
-                to_cp437('|')
-            );
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'));
         }
     }
 
     fn hit_obstacle(&self, player: &Player) -> bool {
         let half_size = self.size / 2;
-        let does_x_match = plater.x == self.x;
+        let does_x_match = player.x == self.x;
         let player_above_gap = player.y < self.gap_y - half_size;
         let player_below_gap = player.y > self.gap_y + half_size;
 
--- src/states.rs
+++ src/states.rs
@@ -1,6 +1,6 @@
 use crate::player::Player;
+use crate::{Obstacle, FRAME_DURATION, SCREEN_HEIGHT, SCREEN_WIDTH};
 use bracket_lib::prelude::*;
-use crate::{FRAME_DURATION, SCREEN_HEIGHT};
 
 enum GameMode {
     Menu,
@@ -11,7 +11,9 @@ enum GameMode {
 pub struct State {
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
     mode: GameMode,
+    score: i32,
 }
 
 impl GameState for State {
@@ -31,7 +33,9 @@ impl State {
         State {
             player: Player::new(5, 25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
             mode: GameMode::Menu,
+            score: 0,
         }
     }
 
@@ -50,8 +54,15 @@ impl State {
 
         self.player.render(ctx);
         ctx.print(0, 0, "Press SPACE to flap.");
+        ctx.print(0, 1, &format!("Score: {}", self.score));
 
-        if self.player.y > SCREEN_HEIGHT {
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
             self.mode = GameMode::End;
         }
     }
@@ -59,7 +70,9 @@ impl State {
     fn restart(&mut self) {
         self.player = Player::new(5, 25);
         self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
         self.mode = GameMode::Playing;
+        self.score = 0;
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -80,6 +93,7 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You are dead!");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
         ctx.print_centered(8, "(P) Play Again");
         ctx.print_centered(9, "(Q) Quit Game");
 
